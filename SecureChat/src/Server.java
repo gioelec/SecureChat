f8e7eb3858561d89b7a68ae70b03aec8aeb42ee3
gioele95 <gioele95@users.noreply.github.com>
2018-05-02 16:24:34 +0200

    Challenge send and receive with the verification of consistency

--- SecureChat/src/Server.java
+++ SecureChat/src/Server.java
@@ -1,11 +1,10 @@
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import cryptoutils.communication.Request;
+import cryptoutils.communication.SecureEndpoint;
 import java.io.InputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
@@ -19,7 +18,9 @@ import java.security.cert.CertificateException;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.*;
+import cryptoutils.messagebuilder.MessageBuilder;
+import cryptoutils.hashutils.HashManager;
+import cryptoutils.cipherutils.CryptoManager;
 
 
 public class Server implements Runnable { //Represents Alice in the protocol specifics
@@ -27,9 +28,12 @@ public class Server implements Runnable { //Represents Alice in the protocol spe
     private Request req;
     private Request myReq;
     private PrivateKey myKey;
+    private byte[] authKey;
+    private byte[] symKey;  //KAB
     private String issuer;
     private Certificate myCertificate;
     private Certificate CACertificate;
+    private int myNonce;
     
     /**
      * 
@@ -63,6 +67,10 @@ public class Server implements Runnable { //Represents Alice in the protocol spe
                 }
                 myReq = generateRequest();
                 oout.writeObject(myReq.getEncrypted(req.getPublicKey()));
+                if(!receiveChallenge(oin)){
+                    System.err.println("Challenge not fulfilled by the other user");
+                    continue;
+                }
                 
             }catch(Exception e){
                 System.err.println(e.getMessage());
@@ -85,6 +93,8 @@ public class Server implements Runnable { //Represents Alice in the protocol spe
      */
     private boolean getRequest(ObjectInputStream obj) throws IOException, ClassNotFoundException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, CertificateException{
         this.req = Request.fromEncryptedRequest((byte []) obj.readObject(),myKey); //first we read the length we expect LBA||nb||S(sb,LBA||nb)
+        this.symKey = req.getSecretKey();
+        
         return (req.verifySignature() && req.verifyCertificate(CACertificate)); //TODO verify name
     }
     
@@ -97,15 +107,24 @@ public class Server implements Runnable { //Represents Alice in the protocol spe
      * @throws SignatureException 
      */
     private Request generateRequest() throws CertificateEncodingException, NoSuchAlgorithmException, InvalidKeyException, SignatureException{
+        this.authKey = CryptoManager.generateAES256RandomSecretKey();
         Request req = new Request(issuer,this.req.getIssuer(),myCertificate,myKey.getEncoded());
-        req.setRandomChallenge();
+        this.myNonce =req.setRandomChallenge();
         req.sign(myKey);
         return req;
     }   
-    private byte[] receiveChallenge(){
-        //TODO
-        return null;
+    private boolean receiveChallenge(ObjectInputStream oin){
+        byte [] decryptedMsg =  SecureEndpoint.secureReceive(oin, symKey, authKey);  //data,kab,mab
+        if(decryptedMsg!=null){
+            int receivedNonce =MessageBuilder.toInt(MessageBuilder.extractFirstBytes(decryptedMsg, 4));
+            return (myReq.getChallengeNonce()== receivedNonce);      
+        }
+        return false;
     }
-        
+    private boolean sendChallenge(ObjectOutputStream oout) throws NoSuchAlgorithmException, InvalidKeyException{
+        byte[] myNonce = MessageBuilder.toByteArray(this.myNonce);
+        byte[] msg = MessageBuilder.concatBytes(myNonce,HashManager.doMAC(myNonce, req.getSecretKey(), SecureEndpoint.AUTH_ALG));
+        return SecureEndpoint.secureSend(msg, oout, symKey, authKey);
+    }
     
 }
